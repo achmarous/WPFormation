61b4866544461af7a1a325f417d89dc8bb957ce5
oussama-mhc <oussamamhc@gmail.com>
2024-09-15 12:16:31 +0100

    BoxMessage Part #9

---
+++ WPFormation/Views/BoxMessage/BoxMessage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFormation.Views.BoxMessage
+{
+    /// <summary>
+    /// Interaction logic for BoxMessage.xaml
+    /// </summary>
+    public partial class BoxMessage : UserControl
+    {
+        public BoxMessage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            /*MessageBox.Show("Could not open file.","ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            */
+
+            MessageBoxResult result = MessageBox.Show("Do you agree?", "Agreement", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                tbinfo.Text = "Agreed";
+
+            }
+            else
+            {
+                tbinfo.Text = "Not Agreed";
+            }
+        }
+    }
+}
